2257d3c30121299335283bdc2c629a7cfbf0232f
Sheraz Butt <sherazbutt592@gmail.com>
2025-08-24 21:18:24 +0500

    Delete GameDataParser directory

--- GameDataParser/GameInformation.cs
+++
@@ -1,6 +0,0 @@
-﻿class GameInformation
-{
-    public string Title { get; set; }
-    public int ReleaseYear { get; set; }
-    public double Rating { get; set; }
-}
--- GameDataParser/StringExtensions.cs
+++
@@ -1,5 +0,0 @@
-﻿static class StringExtensions
-{
-    public static bool IsEmpty(this string str) => str.Length == 0;
-    public static bool IsNull(this string str) => str == null;
-}
